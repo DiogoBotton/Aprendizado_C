19f8028ba5b27c750e6a2e5652a6509b3b67e7e6
DiogoBotton <diogo.luo@gmail.com>
2023-09-22 11:18:58 -0300

    Example arrays and loops with csharp for better debug

---
+++ Csharp/Program.cs
@@ -0,0 +1,24 @@
+﻿// Quando código atualizado, necessário compilar com "dotnet run" e, após isso, apertar F5
+// .vscode/launch.json com configuração para aceitar entrada de dados
+Console.WriteLine("Hello, World!");
+
+// Array de nomes
+string[] nomes = new string[5];
+
+// For para adicionar nomes em cada índice do array
+// nomes.Length retorna comprimento do array, no caso, 5
+for (int i = 0; i < nomes.Length; i++)
+{
+    string nome = Console.ReadLine();
+
+    if(!string.IsNullOrEmpty(nome))
+        nomes[i] = nome;
+}
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Nome {i+1}: {nomes[i]}.");
+}
+
+Console.WriteLine();
+Console.WriteLine("Fim do programa.");
